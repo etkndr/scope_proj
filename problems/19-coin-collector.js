a517cb2f386350bb4330fd70cd93c400df0b8918
etkndr <103692901+etkndr@users.noreply.github.com>
2022-07-16 18:08:25 -0400

    19

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -24,9 +24,20 @@ Example 3:
   console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 
 ***********************************************************************/
+function coinCollector(numCoins) {
+      let arr = []
+  for (i = 0; i < numCoins; i++) {
+  return function(coinVal) {
+    arr.push(coinVal)
+    return arr
+  }
+}
+}
 
-// your code here
-
+let threeCoins = coinCollector(3); // returns a function
+threeCoins(25); // returns a function
+threeCoins(5); // returns a function
+console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
