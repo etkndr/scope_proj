68c28eed2d695c0be4283e0db85b0625702a9ecd
etkndr <103692901+etkndr@users.noreply.github.com>
2022-07-16 17:55:48 -0400

    17

--- problems/17-arrow-reverse.js
+++ problems/17-arrow-reverse.js
@@ -12,8 +12,17 @@ let result3 = arrowReverseString("potato"); // returns "otatop"
 
 ***********************************************************************/
 
-// your code here!
+const arrowReverseString = (str) => {
+  let arr = []
+  for (i = str.length; i >= 0; i--) {
+    arr.push(str[i])
+  }
+  return arr.join("")
+}
 
+let result1 = console.log(arrowReverseString("hello")); // returns "olleh"
+let result2 = arrowReverseString("garden"); // returns "nedrag"
+let result3 = arrowReverseString("potato"); // returns "otatop"
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 
 try {
